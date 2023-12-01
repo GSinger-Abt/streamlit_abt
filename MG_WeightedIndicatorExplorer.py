17113dbd0b896bc7c681267c07dfa186eab3b5e8
GSinger-Abt <151070704+GSinger-Abt@users.noreply.github.com>
2023-12-01 09:06:51 -0500

    Update MG_WeightedIndicatorExplorer.py

--- MG_WeightedIndicatorExplorer.py
+++ MG_WeightedIndicatorExplorer.py
@@ -322,6 +322,7 @@ with tab2:
 with tab3:
     st.title("Indicator Explorer (ArcGIS)")
     experience_builder_url = r'https://experience.arcgis.com/experience/342ca27b75774a02a318f6eb9bb47951'
-    components.iframe(experience_builder_url, width = "1000", height = "1000")
+    components.iframe(experience_builder_url, width = 1000, height = 1000)
+    st.link_button('Click here to open the Indicator Explorer in another window.', experience_builder_url)
     # # Use st.markdown with HTML to embed the website using an iframe
     # st.markdown(f'<iframe src="{experience_builder_url}" width="800" height="600"></iframe>', unsafe_allow_html=True)
